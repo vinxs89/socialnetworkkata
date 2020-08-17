414aee8d495a2df41525ecaa2c7451b23ed3462d
Vincenzo Franco <vinxs89@gmail.com>
2020-08-17 19:30:36 +0200

    Basic maven structure

---
+++ src/main/java/kata/exercise/socialnetwork/Main.java
@@ -0,0 +1,8 @@
+package kata.exercise.socialnetwork;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
---
+++ src/test/java/kata/exercise/socialnetwork/MainTest.java
@@ -0,0 +1,15 @@
+package kata.exercise.socialnetwork;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class MainTest {
+
+    private Main main = new Main();
+
+    @Test
+    void mainExists() {
+        assertNotNull(main);
+    }
+}
