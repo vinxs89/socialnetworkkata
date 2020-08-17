212519b194c072412bbd19e36765e823a13f0f7a
Vincenzo Franco <vinxs89@gmail.com>
2020-08-17 19:58:57 +0200

    Added command recognition

---
+++ src/main/java/kata/exercise/socialnetwork/Command.java
@@ -0,0 +1,25 @@
+package kata.exercise.socialnetwork;
+
+import java.util.regex.Pattern;
+
+public enum Command {
+    POST(Pattern.compile("[0-z]+ -> .+")),
+    READ(Pattern.compile("[0-z]+")),
+    FOLLOW(Pattern.compile("[0-z]+ follows [0-z]+")),
+    WALL(Pattern.compile("[0-z]+ wall"));
+
+    private Pattern pattern;
+
+    Command(Pattern pattern) {
+        this.pattern = pattern;
+    }
+
+    public static Command fromInput(String input) {
+        for(Command command : Command.values()) {
+            if(command.pattern.matcher(input).matches()) {
+                return command;
+            }
+        }
+        return null;
+    }
+}
---
+++ src/test/java/kata/exercise/socialnetwork/CommandTest.java
@@ -0,0 +1,27 @@
+package kata.exercise.socialnetwork;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+public class CommandTest {
+
+    @Test
+    void testCommandRecognition() {
+        assertEquals(Command.POST, Command.fromInput("Alice1 -> Message"));
+        assertEquals(Command.POST, Command.fromInput("1Alice ->  test"));
+        assertEquals(Command.READ, Command.fromInput("Alice"));
+        assertEquals(Command.FOLLOW, Command.fromInput("Alice follows Bob"));
+        assertEquals(Command.WALL, Command.fromInput("Alice wall"));
+        assertNull(Command.fromInput("Alice test"));
+        assertNull(Command.fromInput("Alice test "));
+        assertNull(Command.fromInput(" Alice test"));
+        assertNull(Command.fromInput("Alice Bob -> test"));
+        assertNull(Command.fromInput(" Alice -> test"));
+        assertNull(Command.fromInput("Alice  -> test"));
+        assertNull(Command.fromInput(" Alice follows Bob"));
+        assertNull(Command.fromInput("Alice follows Bob "));
+        assertNull(Command.fromInput("Alice Bob wall"));
+        assertNull(Command.fromInput("Alice wall "));
+    }
+}
